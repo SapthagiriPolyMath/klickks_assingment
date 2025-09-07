98e00b5bd525248bb54628c4b49af279040214e5
Sapthagiri .P <sapthagiri2110@gmail.com>
2025-09-07 23:15:38 +0530

    Updated cors with Vercel frontend deployment link

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const authRoutes = require('./routes/auth');
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://klickks-assingment.vercel.app', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(session({
